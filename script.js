b7411719db2529f40d635340ad91d134639b6b41
Belyonepic <nabiil1709@gmail.com>
2026-04-07 21:20:22 +0700

    added more things to biologi-geografi.html

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+const image = document.getElementById('mainImage');
+const hoverAreas = document.querySelectorAll(".hover-area");
 
-function joinedFor () {
-	
-}
+hoverAreas.forEach(area => {
+	area.addEventListener('mouseenter', () => {
+		//image.src = area.dataset.img;
+
+		//console.log("hover works")
+		//console.log(area.dataset.img);
+		const newSrc = area.getAttribute("data-img");
+    	console.log("SETTING:", newSrc);
+
+    	image.src = newSrc;
+	});
+
+	area.addEventListener('mouseleave', () => {
+		image.src = 'imgs/Poster.png';
+	});
+});
